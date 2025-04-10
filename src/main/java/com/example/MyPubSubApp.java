3a38164bb11cd773b15034c02e9d9ce28f7eb4cd
mpeddada1 <mpeddada@google.com>
2025-04-10 14:21:13 +0000

    virtual thread executor

--- src/main/java/com/example/MyPubSubApp.java
+++ src/main/java/com/example/MyPubSubApp.java
@@ -5,6 +5,8 @@ import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -12,21 +14,16 @@ import java.util.concurrent.Executors;
 @SpringBootApplication
 public class MyPubSubApp {
 
-  PubSubTemplate pubSubTemplate;
 
-  public MyPubSubApp(PubSubTemplate pubSubTemplate) {
-    this.pubSubTemplate = pubSubTemplate;
 
-  }
-
-  @Bean(name = "pubsubAsynchronousPullExecutor")
+  @Bean(name = "pubSubAsynchronousPullExecutor")
   public ExecutorService virtualThreadPullExecutor() {
     return Executors.newVirtualThreadPerTaskExecutor();
   }
 
 
   @Bean
-  public CommandLineRunner commandLineRunner() {
+  public CommandLineRunner commandLineRunner(PubSubTemplate pubSubTemplate) {
 
     return args -> {
       String subscriptionName = "projects/mpeddada-test/subscriptions/exampleSubscription";
